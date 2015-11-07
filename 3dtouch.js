1bba3b154d4254ac36fc57f4d2884a66f87cc62b
Gordon Cohen <gordon993@gmail.com>
2015-11-06 16:25:28 -0800

    2ndelementtake2

--- 3dtouch.js
+++ 3dtouch.js
@@ -1,11 +1,12 @@
-var element4 = document.getElementById('forceMe4');
+var element5 = document.getElementById('forceMe4');
 var element6 = document.getElementById('forceMe6');
 
 var forceValueOutput = document.getElementById('forceValue');
 var background = document.getElementById('background');
 var touch = null;
 
-addForceTouchToElement(element);
+addForceTouchToElement(element4);
+addForceTouchToElement(element6);
 
 function onTouchStart(e) {
   e.preventDefault();
@@ -42,13 +43,13 @@ function refreshForceValue() {
 
 // blur**
 function renderElement(forceValue) {
-  element.style.webkitTransform = 'translateX(-50%) translateY(-50%) scale(' + (1 + forceValue * 1.5) + ')';
+  element4.style.webkitTransform = 'translateX(-50%) translateY(-50%) scale(' + (1 + forceValue * 1.5) + ')';
   background.style.webkitFilter = 'blur(' + forceValue * 50 + 'px)';
   forceValueOutput.innerHTML = 'Force: ' + forceValue.toFixed(4);
 }
 // invert**
 function renderElementInvert(forceValue) {
-  element.style.webkitTransform = 'translateX(-50%) translateY(-50%) scale(' + (1 + forceValue * 1.5) + ')';
+  element6.style.webkitTransform = 'translateX(-50%) translateY(-50%) scale(' + (1 + forceValue * 1.5) + ')';
   background.style.webkitFilter = 'invert(' + forceValue * 80 + '%)';
   forceValueOutput.innerHTML = 'Force: ' + forceValue.toFixed(4);
 }
